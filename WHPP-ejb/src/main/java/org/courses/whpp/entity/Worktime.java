dfeb2ca07ae87aec61fbd305fe27bbec8adecc31
Vladislav <root@stvad.org>
2012-06-21 02:37:36 +0300

    Signin/out functionality moved to WorkTime facade and fixed.

--- WHPP-ejb/src/main/java/org/courses/whpp/entity/Worktime.java
+++ WHPP-ejb/src/main/java/org/courses/whpp/entity/Worktime.java
@@ -33,7 +33,8 @@ import javax.xml.bind.annotation.XmlRootElement;
     @NamedQuery(name = "Worktime.findAll", query = "SELECT w FROM Worktime w"),
     @NamedQuery(name = "Worktime.findById", query = "SELECT w FROM Worktime w WHERE w.id = :id"),
     @NamedQuery(name = "Worktime.findByIntime", query = "SELECT w FROM Worktime w WHERE w.intime = :intime"),
-    @NamedQuery(name = "Worktime.findByOuttime", query = "SELECT w FROM Worktime w WHERE w.outtime = :outtime")
+    @NamedQuery(name = "Worktime.findByOuttime", query = "SELECT w FROM Worktime w WHERE w.outtime = :outtime"),
+    @NamedQuery(name = "Worktime.findOpenedByEmployeeId", query = "SELECT w FROM Worktime w WHERE w.ountime = null and w.EMP_ID = :EmployeeId")
 })
 public class Worktime implements Serializable
 {
@@ -77,6 +78,14 @@ public class Worktime implements Serializable
         this.intime = intime;
         this.outtime = outtime;
     }
+
+    public Worktime(Date intime, Date outtime, Employee empId)
+    {
+        this.intime = intime;
+        this.outtime = outtime;
+        this.empId = empId;
+    }
+
 
     public Integer getId()
     {
--- WHPP-ejb/src/main/java/org/courses/whpp/session/EmployeeFacade.java
+++ WHPP-ejb/src/main/java/org/courses/whpp/session/EmployeeFacade.java
@@ -8,25 +8,36 @@ package org.courses.whpp.session;
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+
 import org.courses.whpp.entity.Employee;
+import org.courses.whpp.entity.Worktime;
+
+import java.util.List;
 
 /**
- *
  * @author Roman Kostyrko <nubaseg@gmail.com>
- * Created on Jun 13, 2012, 8:11:39 PM
+ *         Created on Jun 13, 2012, 8:11:39 PM
  */
 @Stateless
-public class EmployeeFacade extends AbstractFacade<Employee> {
-	@PersistenceContext(unitName = "org.courses_WHPP-ejb_ejb_1.0-SNAPSHOTPU")
-	private EntityManager em;
+public class EmployeeFacade extends AbstractFacade<Employee>
+{
+    @PersistenceContext(unitName = "org.courses_WHPP-ejb_ejb_1.0-SNAPSHOTPU")
+    private EntityManager em;
 
-	@Override
-	protected EntityManager getEntityManager() {
-		return em;
-	}
+    @Override
+    protected EntityManager getEntityManager()
+    {
+        return em;
+    }
 
-	public EmployeeFacade() {
-		super(Employee.class);
-	}
+    public EmployeeFacade()
+    {
+        super(Employee.class);
+    }
+
+    public Employee findById(Integer id)
+    {
+        return (Employee) em.createNamedQuery("Employee.findById").setParameter("id", id).getSingleResult();
+    }
 
 }
